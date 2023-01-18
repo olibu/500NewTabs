e5656e3e74fdb9482329e9f1b18a8ee156ef4d52
Oliver Burgmaier <oliver.burgmaier@gmail.com>
2023-01-18 21:18:21 +0100

    i18n added not working caching added

--- src/newtab.js
+++ src/newtab.js
@@ -8,16 +8,59 @@ async function setBackgroundImage() {
     const backgroundField = document.getElementById('background')
     const pos = Math.floor(Math.random() * options.images.length);
     backgroundField.style.background = "url(" + options.images[pos] + ")";
+
+
+}
+
+function storeImage() {
+    var xhr = new XMLHttpRequest(),
+        blob,
+        fileReader = new FileReader();
+    let src = 'https://drscdn.500px.org/photo/1059807885/q%3D80_m%3D1500/v2?sig=9fdd1bda3de7b75a54ec334e78025f698a80800947fbdd197c2d41cde9fed399';
+    xhr.open("GET", src, true);
+    xhr.responseType = "arraybuffer";
+    xhr.addEventListener("load", function () {
+        if (xhr.status === 200) {
+            // Create a blob from the response
+            blob = new Blob([xhr.response], {type: "image/png"});
+
+            // onload needed since Google Chrome doesn't support addEventListener for FileReader
+            fileReader.onload = function (evt) {
+                // Read out file contents as a Data URL
+                var result = evt.target.result;
+                // Set image src to Data URL
+                rhino.setAttribute("src", result);
+                // Store Data URL in localStorage
+                try {
+                    localStorage.setItem("rhino", result);
+                }
+                catch (e) {
+                    console.log("Storage failed: " + e);
+                }
+            };
+            // Load blob as Data URL
+            fileReader.readAsDataURL(blob);
+        }
+    }, false);
+    // Send XHR
+    xhr.send();
+
 }
 
 async function updateCache() {
-    // TODO: check for last update
+    // check for last update
     if (options.lastUpdate!==-1 && (options.lastUpdate + (1000*60*5)) > new Date().getTime() ) {
         return;
     }
 
-    // update the images in cache (for the next tab)
+    // update the image
     const imageUrls = await getUrls();
+    // const pos = Math.floor(Math.random() * options.images.length);
+    // const nextImageUrl = imageUrls[pos]
+    // backgroundField.style.background = "url(" +  + ")";
+
+
+
     options.images = imageUrls;
 
     // save new list in store
@@ -47,6 +90,20 @@ async function getUrls() {
       return imageUrls;
 }
 
+function convertImageToBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/jpeg");
+
+    return dataURL;
+    // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
 async function setDateGreeting() {
     var timeField = document.getElementById('time');
     var greetingField = document.getElementById('greeting')
@@ -66,11 +123,11 @@ async function setDateGreeting() {
 
 async function greeting(hour) {
     if (hour >= 0 && hour < 12) {
-        return "Good morning, " + options.name;
+        return chrome.i18n.getMessage("greeting_morning", options.name);
     } else if (hour >= 12 && hour < 17) {
-        return "Good afternoon, "  + options.name;
+        return chrome.i18n.getMessage("greeting_afternoon", options.name);
     } else {
-        return "Good evening, " + options.name;
+        return chrome.i18n.getMessage("greeting_evening", options.name);
     }
 }
 
@@ -97,7 +154,11 @@ async function init() {
 
 (function () {
     'use strict';
-
+    
+    
+    storeImage();
     init();
+
+
 })();
   
